4018f46e456ea72043a846b9bacbd25f791c90e7
Eduardo Silveira <silveiraeduardo8@hotmail.com>
2023-09-21 09:44:56 -0600

    invoice automation

---
+++ Python/Timesheet and Invoice Automation/invoice.py
@@ -0,0 +1,88 @@
+import openpyxl
+from datetime import datetime, timedelta
+import win32com.client
+import calendar
+import os
+
+invoiceBaseDir = 'D:\OneDrive\Documents\Contracts\Invoices\\'
+
+excelFile = invoiceBaseDir + 'Invoice_Template.xlsx'
+today = datetime.today().strftime('%d/%m/%Y')
+dt = datetime.strptime(today, '%d/%m/%Y')
+start = dt - timedelta(days=dt.weekday())
+end = start + timedelta(days=6)
+
+wb = openpyxl.load_workbook(excelFile)
+sheet = wb.active
+
+# set end week date on Date field
+sheet[f'E4'].value = end.strftime('%B %d, %Y')
+
+# set end week date on the invoice line
+sheet[f'B17'].value = end.strftime('%B %d, %Y')
+
+# Set invoice #
+invoiceNumber = 'EC2ML_'+ str(calendar.month_abbr[datetime.today().month].upper()) + str(end.day) + str(datetime.today().year)
+sheet[f'E7'].value = invoiceNumber
+
+#counter = sheet[f'B30'].value
+#sheet[f'B30'].value = counter + 1
+
+wb.save(excelFile)
+wb.close()
+
+
+# Print invoice in PDF
+o = win32com.client.Dispatch("Excel.Application")
+o.Visible = False
+wb_path = invoiceBaseDir + 'Invoice_Template.xlsx'
+wb = o.Workbooks.Open(wb_path)
+
+nameOfFile = str(calendar.month_name[datetime.today().month]) + str(end.day) + '_' + str(datetime.today().year) + '.pdf'
+path_to_pdf = invoiceBaseDir + nameOfFile
+print('path to initial pdf: ',path_to_pdf)
+
+wb.WorkSheets([1]).Select()
+wb.ActiveSheet.ExportAsFixedFormat(0, path_to_pdf)
+wb.Close(True, wb_path)
+
+invoiceFolder = invoiceBaseDir + str(calendar.month_name[datetime.today().month]) + str(datetime.today().year)
+print('path to new pdf: ',invoiceFolder)
+
+if os.path.exists(invoiceFolder):
+    print('dir exists - moving file')
+    os.rename(path_to_pdf, invoiceFolder + '\\' + nameOfFile)
+else:
+    print('dir doesnt exists - creating and moving file')
+    os.mkdir(invoiceFolder)
+    os.rename(path_to_pdf, invoiceFolder + '\\' + nameOfFile)
+
+
+
+# Read from timesheet file
+timesheetBaseDir = 'D:\OneDrive\Documents\Contracts\Timesheets\\'
+
+excelFile = invoiceBaseDir + 'Invoice_Template.xlsx'
+today = datetime.today().strftime('%d/%m/%Y')
+dt = datetime.strptime(today, '%d/%m/%Y')
+start = dt - timedelta(days=dt.weekday())
+end = start + timedelta(days=6)
+
+wb = openpyxl.load_workbook(excelFile)
+sheet = wb.active
+
+# set end week date on Date field
+sheet[f'E4'].value = end.strftime('%B %d, %Y')
+
+# set end week date on the invoice line
+sheet[f'B17'].value = end.strftime('%B %d, %Y')
+
+# Set invoice #
+invoiceNumber = 'EC2ML_'+ str(calendar.month_abbr[datetime.today().month].upper()) + str(end.day) + str(datetime.today().year)
+sheet[f'E7'].value = invoiceNumber
+
+#counter = sheet[f'B30'].value
+#sheet[f'B30'].value = counter + 1
+
+wb.save(excelFile)
+wb.close()
